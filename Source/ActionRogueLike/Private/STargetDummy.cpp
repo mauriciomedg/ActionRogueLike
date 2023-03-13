672d0a242b8fee55caadb794ceafaf11ca7aaba3
Mauricio Medina <minimiGamesAM@gmail.com>
2023-03-12 20:42:03 -0400

    Add hitFlash material to flash when the object change health value. There is a potential issues with the Event / Delegate not being triggered or getting crashes.

---
+++ Source/ActionRogueLike/Private/STargetDummy.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+#include "Components/StaticMeshComponent.h"
+#include "SAttributeComponent.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	RootComponent = MeshComp;
+
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+
+	AttributeComp->OnHealthChange.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwingComp, float NewHealth, float Delta)
+{
+	if (Delta < 0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+	}
+}
+
+
+// Called when the game starts or when spawned
+void ASTargetDummy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASTargetDummy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
