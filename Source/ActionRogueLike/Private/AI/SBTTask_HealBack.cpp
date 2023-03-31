0d342e60fd189bbc6fd9c95b63b4c180e67c7bc5
Mauricio Medina <minimiGamesAM@gmail.com>
2023-03-30 21:25:00 -0400

    Assignment4: Add SBTask node to health back the Minion.

---
+++ Source/ActionRogueLike/Private/AI/SBTTask_HealBack.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTask_HealBack.h"
+
+#include "GameFramework/Character.h"
+#include "AIController.h"
+#include "SAttributeComponent.h"
+
+EBTNodeResult::Type USBTTask_HealBack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (ensure(AIController))
+	{
+		ACharacter* Minion = Cast<ACharacter>(AIController->GetPawn());
+
+		if (!Minion)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(Minion->GetComponentByClass(USAttributeComponent::StaticClass()));
+
+		if (AttributeComp)
+		{
+			if (AttributeComp->ApplyHealthChange(100.0f))
+			{
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+
+	return EBTNodeResult::Failed;
+
+}
