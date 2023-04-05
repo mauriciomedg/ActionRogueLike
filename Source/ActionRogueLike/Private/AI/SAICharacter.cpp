d56ff02a1bc4ce52bc2ffd327a4bffb8ec2fc91a
Mauricio Medina <minimiGamesAM@gmail.com>
2023-04-04 22:24:03 -0400

    Lecture13: Add death and ragdoll. Do some cleaning.

--- Source/ActionRogueLike/Private/AI/SAICharacter.cpp
+++ Source/ActionRogueLike/Private/AI/SAICharacter.cpp
@@ -4,6 +4,7 @@
 #include "AI/SAICharacter.h"
 #include "Perception/PawnSensingComponent.h"
 #include "AIController.h"
+#include "BrainComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "DrawDebugHelpers.h"
 #include "SAttributeComponent.h"
@@ -44,10 +45,28 @@ void ASAICharacter::OnHealthChanged(AActor* InstigatorActor, USAttributeComponen
 	{
 		AAIController* AIC = Cast<AAIController>(GetController());
 
-		if (AIC)
+		if (!AIC)
+		{
+			return;
+		}
+
+		if (NewHealth > 0)
 		{
 			UBlackboardComponent* BBC = AIC->GetBlackboardComponent();
 			BBC->SetValueAsFloat("Health", NewHealth);
+		}
+		else
+		{
+			// bot dead //
+			// stop BT
+			AIC->GetBrainComponent()->StopLogic("Killed");
+
+			// Ragdoll
+			GetMesh()->SetAllBodiesSimulatePhysics(true);
+			GetMesh()->SetCollisionProfileName("Ragdoll");
+
+			// set lifespan. This gives time to play ragdoll before desapear 
+			SetLifeSpan(10.0f); 
 		}
 	}
 }
--- Source/ActionRogueLike/Private/SItemChest.cpp
+++ Source/ActionRogueLike/Private/SItemChest.cpp
@@ -4,12 +4,8 @@
 #include "SItemChest.h"
 #include "Components/StaticMeshComponent.h"
 
-// Sets default values
 ASItemChest::ASItemChest()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
 	RootComponent = BaseMesh;
 
@@ -21,18 +17,3 @@ void ASItemChest::Interact_Implementation(APawn* InstigatorPawn)
 {
 	LidMesh->SetRelativeRotation(FRotator(TargetPitch, 0, 0));
 }
-
-// Called when the game starts or when spawned
-void ASItemChest::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ASItemChest::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
